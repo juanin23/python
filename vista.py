28a7fc073806689e030d12c75b0f78fcfdaf5ef6
Juan Carlos Guzman <juanc.23.g@ciencias.unam.mx>
2019-08-20 14:06:32 -0500

    Primer archivo

---
+++ vista.py
@@ -0,0 +1,8 @@
+
+
+print("Probando entrada y salida estandar\nIngresa tu nombre y presiona enter")
+nombre = input()
+print("Ingresa tu edad y presiona enter")
+edad = input()
+print("\nBienvenido " , nombre)
+print("Tu edad es: " , edad)
