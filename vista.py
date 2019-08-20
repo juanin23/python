e417f3af4b1bf8c5e099b1cac276745ceb0ddd1a
Juan Carlos Guzman <juanc.23.g@ciencias.unam.mx>
2019-08-20 14:28:15 -0500

    Mejor formato

--- vista.py
+++ vista.py
@@ -1,8 +1,13 @@
 import datos as dt
 
 print("Probando entrada y salida estandar\nIngresa tu nombre y presiona enter")
+
 nombre = dt.get_nombra()
+
 print("Ingresa tu edad y presiona enter")
+
 edad = dt.get_edad()
+
 print("\nBienvenido " , nombre)
+
 print("Tu edad es: " , edad)
